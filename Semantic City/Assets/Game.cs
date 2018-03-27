44dfdf5e96c780d4352c6d0f9b98c27deeff8bf5
Hizal Celik <hizalcelik@gmail.com>
2018-03-27 15:43:33 -0400

    added unity

---
+++ Semantic City/Assets/Game.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+// run everything
+public class Game : MonoBehaviour {
+
+	void Start () {
+    
+    Gen.City.Generate ().transform.localScale *= 4;
+//		MaterialManager.MaterialInstances();
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+
+		ScreenCapture.CaptureScreenshot ("frame_" + Time.frameCount.ToString () + ".png");
+	}
+}
